2cbfcf3f97bcb11250011396de4f0b19daf69a9a
DanielAlk <daniodfad@yahoo.com.ar>
2015-08-21 10:33:32 -0300

    exito

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821121218) do
+ActiveRecord::Schema.define(version: 20150821120159) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "title",      limit: 255
