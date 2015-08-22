57ab5fe1b6329ae1e77fd4db1fae69bbc95b20fc
DanielAlk <daniodfad@yahoo.com.ar>
2015-08-22 00:38:29 -0300

    destroy SpeakerArea object when destroying Area

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,7 +2,7 @@ class Area < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
 
-  has_many :speaker_areas
+  has_many :speaker_areas, :dependent => :destroy
 	has_many :speakers, through: :speaker_areas
 
 	def slug_candidates
