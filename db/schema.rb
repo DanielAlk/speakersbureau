89d2d2a10e273679f7603c75310cdbbdeaba6d29
DanielAlk <daniodfad@yahoo.com.ar>
2015-09-11 04:58:26 -0300

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,19 +14,19 @@
 ActiveRecord::Schema.define(version: 20150909200848) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",        null: false
-    t.string   "encrypted_password",     limit: 255, default: "",        null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,         null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "profile",                limit: 255, default: "regular"
+    t.string   "profile",                limit: 255
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
